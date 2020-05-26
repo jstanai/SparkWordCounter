8ec085d2480f2e61ff0e672a61ae8f2fcd28dd12
Jared <jcamstan@gmail.com>
2020-05-26 09:21:48 -0600

    Example Text Working

--- vignettes/run.R
+++ vignettes/run.R
@@ -5,9 +5,10 @@ Sys.setenv(JAVA_HOME = "/Library/Java/JavaVirtualMachines/jdk1.8.0_251.jdk/Conte
 sc <- spark_connect(master = "local")
 dir <- "/Users/Jared/GitHub/SparkWordCounter/inst/java/"
 input_path <- paste0(dir, 'input_example.txt')
-output_path <- paste0(dir, 'output_example')
+output_path <- paste0(dir, 'output_example.parquet')
 
 # note: output_path must not already exist!
 count_words(sc, input_path, output_path)
 
-
+# Example of Reading in Results
+df <- spark_read_text(sc, output_path)
